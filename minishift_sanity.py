8fa01a17a8933c150ee502d9b174efdcc6c9ea2e
amitkrout <arout@redhat.com>
2017-02-14 11:30:20 +0530

    Login added to each testcase

--- minishift_sanity.py
+++ minishift_sanity.py
@@ -223,33 +223,33 @@ class minishiftSanity(Test):
             self.fail("Host ping to twitter.com failed")
 
             
-    def atest_python_project(self):
-        new_project(self, self.params.get('openshift_python_PROJECT'), self.params.get('openshift_python_REGISTRY'), self.params.get('service_python_NAME'))
+    def test_python_project(self):
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_python_PROJECT'), self.params.get('openshift_python_REGISTRY'), self.params.get('service_python_NAME'))
         
-    def atest_ruby_project(self):
+    def test_ruby_project(self):
         clean_failed_app(self, self.params.get('openshift_python_PROJECT'))
-        new_project(self, self.params.get('openshift_ruby_PROJECT'), self.params.get('openshift_ruby_REGISTRY'), self.params.get('service_ruby_NAME'))
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_ruby_PROJECT'), self.params.get('openshift_ruby_REGISTRY'), self.params.get('service_ruby_NAME'))
     
-    def atest_perl_project(self):
+    def test_perl_project(self):
         clean_failed_app(self, self.params.get('openshift_ruby_PROJECT'))
-        new_project(self, self.params.get('openshift_perl_PROJECT'), self.params.get('openshift_perl_REGISTRY'), self.params.get('service_perl_NAME'))
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_perl_PROJECT'), self.params.get('openshift_perl_REGISTRY'), self.params.get('service_perl_NAME'))
     
-    def atest_nodejs_project(self):
+    def test_nodejs_project(self):
         clean_failed_app(self, self.params.get('openshift_perl_PROJECT'))
-        new_project(self, self.params.get('openshift_nodejs_PROJECT'), self.params.get('openshift_nodejs_REGISTRY'), self.params.get('service_nodejs_NAME'))
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_nodejs_PROJECT'), self.params.get('openshift_nodejs_REGISTRY'), self.params.get('service_nodejs_NAME'))
     
-    def atest_php_project(self):
+    def test_php_project(self):
         clean_failed_app(self, self.params.get('openshift_nodejs_PROJECT'))
-        new_project(self, self.params.get('openshift_php_PROJECT'), self.params.get('openshift_php_template'), self.params.get('service_php_NAME'), template = True)
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_php_PROJECT'), self.params.get('openshift_php_template'), self.params.get('service_php_NAME'), template = True)
     
-    def atest_nodejs_mongodb_template(self):
+    def test_nodejs_mongodb_template(self):
         clean_failed_app(self, self.params.get('openshift_php_PROJECT'))
-        new_project(self, self.params.get('openshift_nodejsmongodb_PROJECT'), self.params.get('openshift_nodejsmongodb_TEMPLATE'), self.params.get('service_nodejsmongodb_NAME'), template = True, dbservicename = "mongodb")
+        new_project(self, self.params.get('openshift_USER'), self.params.get('openshift_PASSWORD'), self.params.get('openshift_nodejsmongodb_PROJECT'), self.params.get('openshift_nodejsmongodb_TEMPLATE'), self.params.get('service_nodejsmongodb_NAME'), template = True, dbservicename = "mongodb")
     
     def atest_logout(self):
         clean_failed_app(self, self.params.get('openshift_nodejsmongodb_PROJECT'))
         output = minishift.oc_logout(self)
-        logout_str = "Logged " +"\"" +self.params.get('openshift_USERNAME') +"\"" +" out on " +"\"https://"
+        logout_str = "Logged " +"\"" +self.params.get('openshift_USER') +"\"" +" out on " +"\"https://"
         self.assertIn(logout_str, output, "Failed to log out")
 
     def test_ms_stop(self):
